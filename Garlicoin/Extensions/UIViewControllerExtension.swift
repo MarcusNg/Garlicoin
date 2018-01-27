28fd26ef2181262ce03f7dcdca66197dc7b51c2c
Marcus Ng <marcuslistanng@gmail.com>
2018-01-27 18:39:13 -0500

    v1.0

--- Garlicoin/Extensions/UIViewControllerExtension.swift
+++ Garlicoin/Extensions/UIViewControllerExtension.swift
@@ -22,22 +22,6 @@ extension UIViewController {
         view.endEditing(true)
     }
     
-    // Convert hex string to UIColor
-    func colorWithHexString (hex: String) -> UIColor {
-        let cString: String = hex.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines).uppercased()
-        
-        let rString = (cString as NSString).substring(to: 2)
-        let gString = (((cString as NSString).substring(from: 2)) as NSString).substring(to: 2)
-        let bString = (((cString as NSString).substring(from: 4)) as NSString).substring(to: 2)
-        
-        var r: CUnsignedInt = 0, g: CUnsignedInt = 0, b: CUnsignedInt = 0;
-        Scanner(string: rString).scanHexInt32(&r)
-        Scanner(string: gString).scanHexInt32(&g)
-        Scanner(string: bString).scanHexInt32(&b)
-        
-        return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: CGFloat(1))
-    }
-    
     // Store Kit Review Request
     func requestReview() {
         let defaults = UserDefaults.standard
